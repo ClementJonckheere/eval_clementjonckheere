ea5fd7fc2c3c02d7b87ef6d631d6dc06f0f80958
ClementJonckheere <91881425+ClementJonckheere@users.noreply.github.com>
2022-01-12 10:02:50 +0100

    first commit

---
+++ B1_eval_jquery_files/assets/B1_eval_jquery.js
@@ -0,0 +1,50 @@
+$(".redsquare-app").click(function(){
+    $('.redsquare-app.active').removeClass('active');
+    $(this).addClass('active');
+}
+);
+$(".redsquare-controls").attr(disabled,disabled);
+$(".redsquare-controls").removeAttr(disabled);
+$(".btn-light").insertAfter(".btn-primary");
+$(".square").keydown(function(e){
+    switch (e.which){
+    case 37:    // flèche gauche
+        $(".square").finish().animate({
+            left: "-=10"
+        });
+        break;
+    case 38:    // flèche haut
+        $(".square").finish().animate({
+            top: "-=10"
+        });
+        break;
+    case 39:    // flèche droite
+        $(".square").finish().animate({
+            left: "+=10"
+        });
+        break;
+    case 40:    // flèche du bas
+        $(".square").finish().animate({
+            top: "+=10"
+        });
+        break;
+    }
+});
+
+
+
+
+function jumbotron(){
+$('.jumbotron a').remove()
+$('.jumbotron ul').addClass('list-group')
+$('.jumbotron li').addClass('list-group-item')
+$('.jumbotron').before($("li")).addClass('<i class="fa fa-plus-circle"></i>')
+$(document).ready(function(){
+    $(".jambotron")(function(){
+        $("i").children(0).each(function() {           
+            $(this).html($(this).html().replace("inconnue","connue"));
+        });
+    })
+})
+$('.team').prev(1).children(1).addClass("list-group-item-success");
+}
